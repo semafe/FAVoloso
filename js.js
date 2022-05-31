fa7db0ff17f1466cd6f459b712ca802cf243827f
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-05-31 22:11:39 +0200

    Delete js.js

--- js.js
+++
@@ -1,5 +0,0 @@
-function start() {
-    document.getElementById('i-1').style.display='';
-    document.getElementById('start').style.display='none'; 
-    document.getElementById('btn-start').style.display='none';
-}
