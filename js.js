42a502e6f910c2aee4ff4c08539ae436f39a7f62
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-05-31 22:08:59 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+function start() {
+    document.getElementById('i-1').style.display='';
+    document.getElementById('start').style.display='none'; 
+    document.getElementById('btn-start').style.display='none';
+}
