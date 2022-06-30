f9af2f5b86335a8682adbb26a12065326b8729ce
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-07-01 00:28:41 +0200

    js not working

---
+++ mainjs.js
@@ -0,0 +1,43 @@
+function showTwine() {
+    var bar = document.getElementById('demobar');
+    var label = document.getElementById('proTwine');
+    var demo = document.getElementById('twine');
+    bar.style.width = "25%";
+    $( ".progresslabel" ).css("color", "gray");
+    label.style.color = "black";
+    $( ".demoel" ).css("display", "none");
+    demo.style.display = "block";
+}
+
+function showVideo() {
+    var bar = document.getElementById('demobar');
+    var label = document.getElementById('proAnim');
+    var demo = document.getElementById('animation');
+    bar.style.width = "50%";
+    $( ".progresslabel" ).css("color", "gray");
+    label.style.color = "black";
+    $( ".demoel" ).css("display", "none");
+    demo.style.display = "block";
+}
+
+function showUnity() {
+    var bar = document.getElementById('demobar');
+    var label = document.getElementById('proUnity');
+    var demo = document.getElementById('unity');
+    bar.style.width = "75%";
+    $( ".progresslabel" ).css("color", "gray");
+    label.style.color = "black";
+    $( ".demoel" ).css("display", "none");
+    demo.style.display = "block";
+}
+
+function showWirefr() {
+    var bar = document.getElementById('demobar');
+    var label = document.getElementById('proWirefr');
+    var demo = document.getElementById('wireframe');
+    bar.style.width = "100%";
+    $( ".progresslabel" ).css("color", "gray");
+    label.style.color = "black";
+    $( ".demoel" ).css("display", "none");
+    demo.style.display = "block";
+}
