9a52f1faf336aaec91d54d47ad6168db8da00f03
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-07-12 10:20:32 +0200

    Delete FAVoDemoVid.mp4

---
+++ prototype/Maze/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueSystem: MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public Transform dialogueBoxGUI;
+
+    public float letterDelay = 0.1f;
+    public float letterMultiplier = 0.5f;
+
+    public KeyCode DialogueInput = KeyCode.F;
+
+    public string Names;
+
+    public string[] dialogueLines;
+
+    public bool letterIsMultiplied = false;
+    public bool dialogueActive = false;
+    public bool dialogueEnded = false;
+    public bool outOfRange = true;
+
+    void Start()
+    {
+        /*nameText.text = "";*/
+        dialogueText.text = "";
+        dialogueText.fontSize = 13;
+    }
+
+    public void EnterRangeOfNPC()
+    {
+        outOfRange = false;
+            
+    }
+
+    public void NPCName(bool autoTrigger)
+    {
+        outOfRange = false;
+        dialogueBoxGUI.gameObject.SetActive(true);
+        nameText.text = Names;
+        if ((Input.GetKeyDown(KeyCode.F)) || autoTrigger == true)
+        {
+            if (!dialogueActive)
+            {
+                dialogueActive = true;
+                StartCoroutine(StartDialogue());
+            }
+        }
+        StartDialogue();
+    }
+
+    private IEnumerator StartDialogue()
+    {
+        if (outOfRange == false)
+        {
+            int dialogueLength = dialogueLines.Length;
+            int currentDialogueIndex = 0;
+            while (currentDialogueIndex < dialogueLength || !letterIsMultiplied)
+            {
+                if (!letterIsMultiplied)
+                {
+                    letterIsMultiplied = true;
+                    StartCoroutine(DisplayString(dialogueLines[currentDialogueIndex++]));
+
+                    if (currentDialogueIndex >= dialogueLength)
+                    {
+                        dialogueEnded = true;
+                    }
+                }
+                yield return 0;
+            }
+
+            while (true)
+            {
+                if (Input.GetKeyDown(DialogueInput) && dialogueEnded == false)
+                {
+                    break;
+                }
+                yield return 0;
+            }
+            dialogueEnded = false;
+            dialogueActive = false;
+            DropDialogue();
+        }
+    }
+
+    private IEnumerator DisplayString(string stringToDisplay)
+    {
+        if (outOfRange == false)
+        {
+ /*           int stringLength = stringToDisplay.Length;
+            int currentCharacterIndex = 0;*/
+
+            dialogueText.text = stringToDisplay;
+            dialogueEnded = false;
+            yield return new WaitForSeconds(letterDelay);
+
+/*            while (currentCharacterIndex < stringLength)
+            {
+                dialogueText.text += stringToDisplay[currentCharacterIndex];
+                currentCharacterIndex++;
+
+                if (currentCharacterIndex < stringLength)
+                {
+                    if (Input.GetKey(DialogueInput))
+                    {
+                        yield return new WaitForSeconds(letterDelay * letterMultiplier);
+                    }
+                    else
+                    {
+                        yield return new WaitForSeconds(letterDelay);
+                    }
+                }
+                else
+                {
+                    dialogueEnded = false;
+                    break;
+                }
+            }*/
+            while (true)
+            {
+                if (Input.GetKeyDown(DialogueInput))
+                {
+                    Debug.Log("Input ricevuto");
+                    break;
+                }
+                yield return 0;
+            }
+            dialogueEnded = false;
+            letterIsMultiplied = false;
+            dialogueText.text = "";
+        }
+    }
+
+    public void DropDialogue()
+    {
+        dialogueBoxGUI.gameObject.SetActive(false);
+    }
+
+    public void OutOfRange()
+    {
+        outOfRange = true;
+        if (outOfRange == true)
+        {
+            letterIsMultiplied = false;
+            dialogueActive = false;
+            StopAllCoroutines();
+            DropDialogue();
+        }
+    }
+}
---
+++ prototype/Maze/Assets/Scripts/NPC.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class NPC : MonoBehaviour {
+    public bool autoTrigger = false;
+    public Transform ChatBackGround;
+    public Transform NPCCharacter;
+
+    public DialogueSystem dialogueSystem;
+
+    public string Name;
+
+    [TextArea(40, 10)]
+    public string[] sentences;
+
+	void Update () {
+          Vector3 Pos = Camera.main.WorldToScreenPoint(NPCCharacter.position);
+          Pos.y += 175;
+          ChatBackGround.position = Pos;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        Debug.Log("TriggerStay " + this.Name);
+        this.gameObject.GetComponent<NPC>().enabled = true;
+        dialogueSystem.EnterRangeOfNPC();
+        if ((Input.GetKeyDown(KeyCode.F)) || autoTrigger == true)
+        {
+            this.gameObject.GetComponent<NPC>().enabled = true;
+            dialogueSystem.Names = Name;
+            dialogueSystem.dialogueLines = sentences;
+            dialogueSystem.NPCName(autoTrigger);
+            autoTrigger = false;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        dialogueSystem.OutOfRange();
+        this.gameObject.GetComponent<NPC>().enabled = false;
+    }
+}
